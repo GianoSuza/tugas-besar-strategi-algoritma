03fde355f266e83b4473969eedd981bd5ff5793e
Sakie Giano Suza <suzasakie@gmail.com>
2024-05-25 20:48:29 +0700

    beres

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,202 @@
+"use client"
+import React, { useState } from "react";
+import { Source_Serif_4, Montserrat } from "next/font/google";
 import Image from "next/image";
 
-export default function Home() {
+const fontTitle = Source_Serif_4({ subsets: ["latin"] });
+const fontBody = Montserrat({ subsets: ["latin"] });
+
+function mainPage() {
+  const [n, setn] = useState(0)
+  const [k, setk] = useState(0)
+  const [timeBrute, setTimeBrute] = useState(0)
+  const [timeDynamic, setTimeDynamic] = useState(0)
+
+  function measureTime(func) {
+    const start = performance.now();
+    const res = func();
+    const end = performance.now();
+    return end - start;
+  }
+
+  function eggDrop(n, k) {
+    // If there are no floors, then 
+    // no trials needed. OR if there 
+    // is one floor, one trial needed. 
+    if (k == 1 || k == 0)
+      return k;
+
+    // We need k trials for one egg 
+    // and k floors 
+    if (n == 1)
+      return k;
+
+    let min = Number.MAX_VALUE;
+    let x, res;
+
+    // Consider all droppings from 
+    // 1st floor to kth floor and 
+    // return the minimum of these 
+    // values plus 1.
+    for (x = 1; x <= k; x++) {
+      res = Math.max(eggDrop(n - 1, x - 1),
+        eggDrop(n, k - x));
+      if (res < min)
+        min = res;
+    }
+    return min + 1;
+  }
+
+  function max(a, b) {
+    return (a > b) ? a : b;
+  }
+
+  /* Function to get minimum number
+   of trials needed in worst
+      case with n eggs and k floors */
+  function eggDrop2(n, k) {
+    /* A 2D table where entry eggFloor[i][j]
+ will represent minimum number of trials
+needed for i eggs and j floors. */
+    let eggFloor = new Array(n + 1);
+    for (let i = 0; i < (n + 1); i++) {
+      eggFloor[i] = new Array(k + 1);
+    }
+    let res;
+    let i, j, x;
+
+    // We need one trial for one floor and
+    // 0 trials for 0 floors
+    for (i = 1; i <= n; i++) {
+      eggFloor[i][1] = 1;
+      eggFloor[i][0] = 0;
+    }
+
+    // We always need j trials for one egg
+    // and j floors.
+    for (j = 1; j <= k; j++)
+      eggFloor[1][j] = j;
+
+    // Fill rest of the entries in table using
+    // optimal substructure property
+    for (i = 2; i <= n; i++) {
+      for (j = 2; j <= k; j++) {
+        eggFloor[i][j] = Number.MAX_VALUE;
+        for (x = 1; x <= j; x++) {
+          res = 1 + max(
+            eggFloor[i - 1][x - 1],
+            eggFloor[i][j - x]);
+          if (res < eggFloor[i][j])
+            eggFloor[i][j] = res;
+        }
+      }
+    }
+
+    // eggFloor[n][k] holds the result
+    return eggFloor[n][k];
+  }
+
+  const calculate = () => {
+    setTimeBrute(measureTime(() => eggDrop(n, k)))
+    setTimeDynamic(measureTime(() => eggDrop2(n, k)))
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="bg-[#ffc773] w-full flex flex-col items-center p-4 justify-center">
+        <div className="flex mt-16 w-[80%] gap-x-6">
+          <div>
+            <Image src="/telur.png" width={180} height={180} />
+          </div>
+          <div className="w-full flex justify-center items-center mb-16">
+            <div className="">
+              <div className={fontTitle.className}>
+                <div className="text-[70px] font-[650] text-[#422f01] mt-6">
+                  Egg Dropping Puzzle
+                </div>
+              </div>
+              <div className={fontBody.className}>
+                <div className="text-[#422f01] text-2xl mt-[15px] text-justify p-4 font-[460]">
+                  In the egg drop problem, you have{" "}
+                  <span className="font-bold">N</span> eggs and you want to
+                  determine from which floors in a{" "}
+                  <span className="font-bold">K</span> floor building you can
+                  drop an egg such that is doesn't break. You are to determine
+                  the minimum number of attempts you need in order to find the
+                  critical floor in the worst case while using the best
+                  strategy.
+                </div>
+              </div>
+              <div className={fontBody.className}>
+                <div className="text-[#422f01] text-2xl mt-1 text-justify p-4 font-[650]">
+                  Rules:
+                  <li>An egg that survives a fall can be used again.</li>
+                  <li>A broken egg must be discarded.</li>
+                  <li>The effect of a fall is the same for all eggs.</li>
+                  <li>
+                    If an egg breaks when dropped, then it would break if
+
+                    dropped from a higher floor.
+                  </li>
+                  <li>
+                    If an egg survives a fall then it would survive a shorter
+                    fall.
+                  </li>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-[#ffdf92] flex w-full items-end justify-center gap-x-[200px]">
+        <div className="border-[11px] border-solid border-[#927d4a] px-10 py-6 rounded-[35px] mt-16 mb-12">
+          <div className={fontTitle.className}>
+            <div className="text-[55px] font-[590] text-[#422f01] mb-4">
+              Input
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <input onChange={(ev) => setn(ev.target.value)} type="number" className="border-4 border-solid border-[#927d4a] text-black p-4 rounded-[30px] bg-[#fff8e0] text-center" placeholder="number of eggs" />
+            <input onChange={(ev) => setk(ev.target.value)} type="number" className="border-4 border-solid border-[#927d4a] text-black p-4 rounded-[30px] bg-[#fff8e0] mt-4 text-center" placeholder="number of floors" />
+          </div>
+          <div className="w-full flex justify-center">
+            <button onClick={calculate} className="p-2 rounded-3xl bg-[#422f01] text-white w-3/4 mt-4 font-[700]">Enter</button>
+          </div>
+        </div>
+        <div>
+          <Image src="/gedung.png" width={300} height={300} />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="bg-[#ffe9b4] flex w-full items-center justify-between px-[225px]">
+        <div>
+          <Image src="/waktu.png" width={250} height={250} />
+        </div>
+        <div className="flex flex-col justifiy-end items-end">
+          <div className={fontTitle.className}>
+            <div className="text-[55px] font-[590] text-[#422f01] mb-4">
+              Time Complexity
+            </div>
+          </div>
+          <div className="flex">
+            <div className={fontTitle.className}>
+              <div className="text-[33px] font-[400] text-[#422f01] mr-2">
+                Brute Force   :
+              </div>
+            </div>
+            <input value={timeBrute} disabled type="number" className="text-black w-80 border-4 border-solid border-[#422f01] p-4 rounded-[20px] bg-[#fff8e0] text-center" />
+          </div>
+          <div className="flex mt-4">
+            <div className={fontTitle.className}>
+              <div className="text-[33px] font-[400] text-[#422f01] mr-2">
+                Dynamic Programming :
+              </div>
+            </div>
+            <input value={timeDynamic} disabled type="number" className="text-black w-80 border-4 border-solid border-[#422f01] p-4 rounded-[20px] bg-[#fff8e0] text-center" />
+          </div>
+        </div>
       </div>
-    </main>
+    </>
   );
 }
+
+export default mainPage;
